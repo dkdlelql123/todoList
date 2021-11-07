9347597dc27fca0e3c934ce0ca92a6c9f3e6427a
NamYeJi <dkdlelql123@naver.com>
2021-11-07 14:57:21 +0900

    테일윈드, 데이지유아이 설치 및 cdn연결 / 필요없는 파일 삭제 및 투두리스트 코드

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { RecoilRoot } from 'recoil'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import './index.css'
 import App from './App'
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    
+    <RecoilRoot> 
+      <QueryClientProvider client={queryClient}>
+        <App /> 
+      </QueryClientProvider>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
 )
